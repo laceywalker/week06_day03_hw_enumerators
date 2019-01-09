957d9bfdf125b2956d9c8ef4ea08f906913270ba
lacey <hiu@example.com>
2019-01-09 17:58:57 +0000

    pass test 5

--- models/traveller.js
+++ models/traveller.js
@@ -25,10 +25,18 @@ Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
 };
 
 
-// Traveller.prototype.calculateTotalDistanceTravelled = function () {
-//
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((total, journey) => {
+    return total += journey.distance;
+  }, 0);
+};
+
+// Cinema.prototype.totalRunningTime = function () {
+//   return this.films.reduce((total, film) => {
+//     return total += film.length;
+//   }, 0);
 // };
-//
+
 // Traveller.prototype.getUniqueModesOfTransport = function () {
 //
 // };
